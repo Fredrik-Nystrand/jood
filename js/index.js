82d1a749718a6e52cbe6cd9a84442cf89ecd80b0
Fredrik Nystrand <f.nystrand@gmail.com>
2021-11-24 14:27:44 +0100

    lade till mobilmeny

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-/* Framtid kod för mobilmenyn här nedan */
+/* Mobil meny knapp */
+const btnMobileMenu = document.querySelector('#btn-mobile-menu');
+const body = document.querySelector('#body');
+
+/* Över denna skärmstorlek så ska menyn stängas */
+const mobileBreakpoint = 768;
+
+/* lyssna efter klick-event på meny-knappen */
+btnMobileMenu.addEventListener('click', () => {
+    /* Kolla om knappen faktiskt är synlig innan vi går vidare */
+    if (window.getComputedStyle(btnMobileMenu).display === 'flex' && window.innerWidth < mobileBreakpoint) {
+        /* Toggla en klass i body som talar om för css att menyn är öppen eller stängd */
+        body.classList.toggle('mobile-menu-open');
+
+    }
+
+})
+
+/* Stäng menyn om skärmstorleken ökar över breakpointen */
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= mobileBreakpoint) {
+        body.classList.remove('mobile-menu-open');
+    }
+});
